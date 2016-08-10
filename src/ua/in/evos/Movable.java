cbf22472d20da619dbc98a5938d72991bf876893
Nikolay31289 <nikolay31289@ukr.net>
2016-08-11 00:18:06 +0300

    First commit

--- src/ua/in/evos/Movable.java
+++ src/ua/in/evos/Movable.java
@@ -2,8 +2,8 @@ package ua.in.evos;
 
 
 public interface Movable {
-    public static final String STEP_FORWARD_COMMAND = "StepForward";
-    public static final String STEP_BACKWARD_COMMAND = "StepBackward";
+    String STEP_FORWARD_COMMAND = "StepForward";
+    String STEP_BACKWARD_COMMAND = "StepBackward";
     void stepForward(int index);
     void stepBackward(int index);
 
